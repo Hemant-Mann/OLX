a41ff876414ff4d8396376ea569452d0fc27dbb2
Hemant-Mann <hemant.mann121@gmail.com>
2015-02-28 19:16:13 +0530

    removing unused validations

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -58,71 +58,13 @@ $('#registerForm').on('submit', function(event) {
 		password = $('#password').val(),
 		confirmation = $('#confirmation').val();
 	
-	var emailRegExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-	var message = '<p class="message">';
 	
-	// Check if the phone number is valid
-	if((!isNaN(phone)) && (phone.length != 10)) {
-		message += 'Not a valid phone number<br />';
-		$('#message').html(message);
-	} else {
-		message += "";
-	}
+
 	
-	// Checking if the email matches the format
-	if(!emailRegExp.test(email)) {
-		message += 'Not a valid email address';
-		$('#message').html(message);
-	} else {
-		message += "";
-	}
-
-	message += '</p>';
-
-	// If no pre-errors then submit the form
-	if(message == '<p class="message"></p>') {
-		$.ajax({
-			url: form.attr('action'),
-			type: 'POST',
-			data: {submit: true, first_name: firstName, last_name: lastName, phone: phone, email: email, username: username, password: password, confirmation: confirmation},
-		})
-
-		// Done submitting the data
-		.done(function(data) {
-			var content = $(data).find('#message');
-			$('#message').html(content);
-		})
-
-		// Failure
-		.fail(function(data) {
-			var content = $(data).find('#message');
-			$('#message').html(content);
-		})
-		.always(function() {
-			console.log("complete");
-		});	
-	}	
-});
-
-/*
-// Submitting the Product upload form using AJAX
-$('#newProductForm').on('submit', function(event) {
-	event.preventDefault();
-
-	var form = $('#newProductForm'),
-		productCategory = form.find('input[name="category"]:checked').val(),
-		productName = $('#productName').val(),
-		productPrice = $('#productPrice').val(),
-		purchaseYear = $('#purchaseYear').val(),
-		productDescription = $('#productDescription').val();
-
-		console.log(formData);
-
 	$.ajax({
 		url: form.attr('action'),
 		type: 'POST',
-		data: {submit: true, name: productName, price: productPrice, pur_year: purchaseYear, description: productDescription},
+		data: {submit: true, first_name: firstName, last_name: lastName, phone: phone, email: email, username: username, password: password, confirmation: confirmation},
 	})
 
 	// Done submitting the data
@@ -138,6 +80,7 @@ $('#newProductForm').on('submit', function(event) {
 	})
 	.always(function() {
 		console.log("complete");
-	});		
+	});	
+		
 });
-*/
+
